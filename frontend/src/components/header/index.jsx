b9d7a3631de6d0785c439cc3cd13511c2d278886
arthur94a <arthuraraujo94@hotmail.com>
2025-12-06 20:05:18 -0300

    Add Nginx to serve app frontend

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './styles.module.scss'
 
 export function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <h1>Text Match</h1>
             <p>Comparador de textos ".pdf" com python</p>
         </header>
