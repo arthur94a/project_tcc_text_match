c0eda9b97da8126a7aa4d6cc7a02d3dfa48695c2
arthur94a <arthuraraujo94@hotmail.com>
2025-12-06 20:36:29 -0300

    set path alias

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "src") },
+      { find: "@components", replacement: path.resolve(__dirname, "src/components") },
+      { find: "@themes", replacement: path.resolve(__dirname, "src/themes") },
+    ],
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
+  
 })
